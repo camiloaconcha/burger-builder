971a9ced9b88feeacebc483fb42c1f00aa89fa92
Camilo Andres Concha Agudelo <cconcha@belatrix.com>
2019-08-21 15:57:43 -0500

    Added the build controls

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import classes from './BuildControls.css'
+
+const BuildControls = props => {
+    return (
+        <div className={classes.BuildControls}>
+            
+        </div>
+    );
+};
+
+
+
+export default BuildControls;
